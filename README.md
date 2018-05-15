684e6b8304c3f85c705bebb2812522bb54a98a11
gfw-breaker <lixiaoming0513@gmail.com>
2018-05-15 13:52:32 +0800

    o

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # youtube-stream
-Youtube 视频流服务器，可播放，可下载
 
-
-#####  演示站:  [YouTube](https://github.com/gfw-breaker/open-proxy#%E5%AE%89%E8%A3%85%E9%83%A8%E7%BD%B2%E8%AF%B7%E5%8F%82%E8%80%83-open-proxy-%E6%90%AD%E5%BB%BA%E6%95%99%E7%A8%8B) ;  安装部署，请参考 [YouTube代理搭建教程](https://github.com/gfw-breaker/youtube-stream/wiki)
+You2PHP 视频流服务器
--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 # author: gfw-breaker
 
 server_home=/usr/local/youtube-stream
-git_url="https://raw.githubusercontent.com/gfw-breaker/ssr-accounts/master/README.md"
 
 ## install system dependencies
 yum install -y python python-pip vim sysstat
@@ -11,20 +10,16 @@ yum install -y python python-pip vim sysstat
 pip install flask pafy youtube-dl requests py_lru_cache
 
 ## deploy code
-server_ip=$(ifconfig | grep "inet addr" | sed -n 1p | cut -d':' -f2 | cut -d' ' -f1)
-#portal_ip=$(curl -s ${git_url} | grep 8888 | cut -d'/' -f3 | cut -d':' -f1)
-
-sed -i "s/local_server_ip/${server_ip}/g" server.py
-for f in $(ls templates/*.html); do
-    sed -i "s/local_server_ip/${server_ip}/g" ${f}
-done
 mkdir -p ${server_home}
 cp -R * ${server_home}
+
+ipaddr=$(ifconfig | grep "inet addr" | sed -n 1p | cut -d':' -f2 | cut -d' ' -f1)
+sed -i "s/localhost/$ipaddr/g" ${server_home}/upstreams.json
 
 ## enable and start service
 chmod +x yt-stream
 cp yt-stream /etc/init.d
 chkconfig yt-stream on
-service yt-stream start
+service yt-stream restart
 
 
