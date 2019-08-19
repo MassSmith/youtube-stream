97efe99d4a964190a1d9ab935c7a22eb7e779686
MassSmith <43735033+MassSmith@users.noreply.github.com>
2019-08-19 18:51:43 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ server_ip=$(curl -4 ip.sb)
 #server_ip=$(ifconfig | grep "inet addr" | sed -n 1p | cut -d':' -f2 | cut -d' ' -f1)
 #portal_ip=$(curl -s ${git_url} | grep 8888 | cut -d'/' -f3 | cut -d':' -f1)
 
-sed -i "s/local_server_ip/${server_ip}/g" server.py
+#sed -i "s/local_server_ip/${server_ip}/g" server.py
 for f in $(ls templates/*.html); do
     sed -i "s/local_server_ip/${server_ip}/g" ${f}
 done
